7a03378611f17e76551cd573d0c65cd8decd1699
Tavilo Breno <breno_wk2@hotmail.com>
2025-10-14 20:12:27 -0300

    Refactor web routes to enhance authentication handling and organize Nota Fiscal routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\NotaFiscalController;
 
@@ -23,13 +24,30 @@ Route::get('/login', function () {
     return view('login');
 })->name('login');
 
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/');
+})->name('logout');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware('auth')
     ->name('dashboard');
 
-Route::post('/notas', [NotaFiscalController::class, 'store']);
+// API Routes (manter compatibilidade)
+Route::post('/api/notas', [NotaFiscalController::class, 'storeApi'])->name('api.notas.store');
 Route::post('/notas/{id}/cancelar', [NotaFiscalController::class, 'cancelar']);
 Route::post('/notas/inutilizar', [NotaFiscalController::class, 'inutilizar']);
 Route::get('/notas/inutilizacoes', [NotaFiscalController::class, 'consultarInutilizacoes']);
 Route::post('/notas/{id}/correcao', [NotaFiscalController::class, 'emitirCartaCorrecao']);
 Route::post('/notas/{id}/manifestar', [NotaFiscalController::class, 'manifestar']);
+
+// Web Routes
+Route::middleware('auth')->group(function () {
+    Route::get('/notas', [NotaFiscalController::class, 'index'])->name('notas.index');
+    Route::get('/notas/criar', [NotaFiscalController::class, 'create'])->name('notas.create');
+    Route::post('/notas', [NotaFiscalController::class, 'store'])->name('notas.store');
+    Route::get('/notas/{id}', [NotaFiscalController::class, 'show'])->name('notas.show');
+    Route::get('/notas/{id}/download', [NotaFiscalController::class, 'downloadXml'])->name('notas.download');
+});
